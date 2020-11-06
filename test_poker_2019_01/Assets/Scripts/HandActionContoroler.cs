8287a8b33c485809cef4cc569403ab4210f22771
ttteeesstt0123456 <f434@hotmail.com>
2020-11-07 01:47:11 +0900

    2020/11/6 merge

---
+++ test_poker_2019_01/Assets/Scripts/HandActionContoroler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandActionContoroler : MonoBehaviour
+{
+
+    //登録ボタンの定義
+
+
+//フラグの宣言
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
